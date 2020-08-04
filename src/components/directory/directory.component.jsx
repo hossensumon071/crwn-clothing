78b2aa0b48ce749c2743fba6ad682e8dbb548c8a
hossensumon071 <hossensumon071@gmail.com>
2020-08-04 09:27:47 +0600

    update our menu-item to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,29 +14,34 @@ class Directory extends React.Component {
         {
           title: 'hats',
           imageUrl: 'https://images.unsplash.com/photo-1572307480813-ceb0e59d8325?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80',
-          id: 1
+          id: 1,
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
           imageUrl: 'https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-          id: 2
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=343&q=80',
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'womens',
           imageUrl: 'https://images.unsplash.com/photo-1498746607408-1e56960e3bdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: 'mens',
           imageUrl: 'https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         },
       ]
      }
@@ -46,8 +51,8 @@ class Directory extends React.Component {
       return (
         <div className="directory-menu">  
            {
-             this.state.sections.map(({title, imageUrl, id, size}) => (
-               <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} /> 
+             this.state.sections.map(({id, ...otherSectionProps}) => (
+               <MenuItem key={id} {...otherSectionProps} /> 
              ))
            }
         </div>
